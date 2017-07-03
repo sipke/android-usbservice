802cce8637493a02c39776c9b5763e78369d62dc
Sipke Vriend <sipke.vriend@gmail.com>
2017-07-03 20:55:56 +1000

    Add file which make up the UsbService library. Ported from SerialPortExample from github.com/felHR85. The Service is pretty much was in the original example, but the UsbConnectionMonitor is a context wrapper which allows you to keep the usb connection events happening in this class rather than having to hard code it into your main activity. The addition of the FileLogger in the usb service makes for a simple way to add the ability to log the incoming traffic to a file.

---
+++ src/main/java/com/felhr/usbservice/UsbConnectionMonitor.java
@@ -0,0 +1,116 @@
+package com.felhr.usbservice;
+
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.widget.Toast;
+
+import com.felhr.usbserial.UsbSerialInterface;
+
+import java.util.Set;
+
+/*
+ * Utility/Helper class that can be used in a MainActivity to setup a UsbService and catch its
+ * corresponding broadcasts and display to user.
+ * Class which are the UI feedback parts originally inside the MainActivity of the
+ * SerialPortExample by fehlr. Placing them into their own module for reuse through a library.
+ */
+public class UsbConnectionMonitor extends ContextWrapper {
+
+    private UsbService usbService;
+    private Handler mDataHandler;
+    private UsbSerialInterface.UsbReadCallback mCallback;
+
+    public UsbConnectionMonitor(Context base) {
+        super(base);
+    }
+
+    public void start(Handler dataHandler, UsbSerialInterface.UsbReadCallback callback) {
+        mDataHandler = dataHandler;
+        mCallback = callback;
+        setFilters();  // Start listening notifications from UsbService
+        startService(UsbService.class, usbConnection, null); // Start UsbService(if it was not started before) and Bind it
+    }
+
+    public void stop() {
+        unregisterReceiver(mUsbReceiver);
+        unbindService(usbConnection);
+    }
+
+    public UsbService getUsbService() {
+        return usbService;
+    }
+
+    /*
+     * Notifications from UsbService will be received here.
+     */
+    private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            switch (intent.getAction()) {
+                case UsbService.ACTION_USB_PERMISSION_GRANTED: // USB PERMISSION GRANTED
+                    Toast.makeText(context, "USB Ready", Toast.LENGTH_SHORT).show();
+                    break;
+                case UsbService.ACTION_USB_PERMISSION_NOT_GRANTED: // USB PERMISSION NOT GRANTED
+                    Toast.makeText(context, "USB Permission not granted", Toast.LENGTH_SHORT).show();
+                    break;
+                case UsbService.ACTION_NO_USB: // NO USB CONNECTED
+                    Toast.makeText(context, "No USB connected", Toast.LENGTH_SHORT).show();
+                    break;
+                case UsbService.ACTION_USB_DISCONNECTED: // USB DISCONNECTED
+                    Toast.makeText(context, "USB disconnected", Toast.LENGTH_SHORT).show();
+                    break;
+                case UsbService.ACTION_USB_NOT_SUPPORTED: // USB NOT SUPPORTED
+                    Toast.makeText(context, "USB device not supported", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    };
+
+    private void setFilters() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(UsbService.ACTION_USB_PERMISSION_GRANTED);
+        filter.addAction(UsbService.ACTION_NO_USB);
+        filter.addAction(UsbService.ACTION_USB_DISCONNECTED);
+        filter.addAction(UsbService.ACTION_USB_NOT_SUPPORTED);
+        filter.addAction(UsbService.ACTION_USB_PERMISSION_NOT_GRANTED);
+        registerReceiver(mUsbReceiver, filter);
+    }
+
+    private void startService(Class<?> service, ServiceConnection serviceConnection, Bundle extras) {
+        if (!UsbService.SERVICE_CONNECTED) {
+            Intent startService = new Intent(this, service);
+            if (extras != null && !extras.isEmpty()) {
+                Set<String> keys = extras.keySet();
+                for (String key : keys) {
+                    String extra = extras.getString(key);
+                    startService.putExtra(key, extra);
+                }
+            }
+            startService(startService);
+        }
+        Intent bindingIntent = new Intent(this, service);
+        bindService(bindingIntent, serviceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    private final ServiceConnection usbConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName arg0, IBinder arg1) {
+            usbService = ((UsbService.UsbBinder) arg1).getService();
+            usbService.setHandler(mDataHandler);
+            usbService.setReadCallback(mCallback);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            usbService = null;
+        }
+    };
+}
